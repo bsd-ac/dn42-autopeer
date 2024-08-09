c7c8f18caaccb60089438a32c95cc489312a7218
Aisha Tammy <aisha@bsd.ac>
2024-08-09 17:38:57 +0000

    logger and cache

---
+++ autopeer/__init__.py
@@ -0,0 +1,19 @@
+import logging
+import logging.handlers
+from typing import Dict
+
+logger : logging.Logger = logging.getLogger("autopeer")
+logger.setLevel(logging.DEBUG)
+
+formatter : logging.Formatter = logging.Formatter("%(asctime)s [%(levelname)s]\t%(name)s: %(message)s")
+
+syslog_handler : logging.handlers.SysLogHandler = logging.handlers.SysLogHandler(address="/dev/log")
+syslog_handler.setFormatter(formatter)
+logger.addHandler(syslog_handler)
+
+console_handler : logging.StreamHandler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+cache: Dict[int, str] = {}
