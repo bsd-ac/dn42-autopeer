64016d6689205e36d7a0ff02feed40370333ec0d
Aisha Tammy <aisha@bsd.ac>
2024-08-22 05:51:01 +0000

    peer_manager

--- autopeer/peer_manager.py
+++ autopeer/peer_manager.py
@@ -1,12 +1,17 @@
+import base64
+import ipaddress
 import json
 import os
 import socket
 import subprocess
-from typing import Generator
+from typing import Generator, Optional
 
-from . import logger, max_bytes
+from fastapi import HTTPException
 
-# from .templates import bgp_default, bgp_listener, bgp_peer, hostname_wg
+from . import max_bytes
+from .logger import logger
+from .schemas import PeerInfo
+from .templates import bgpd_conf, hostname_wg
 
 
 class PeerManager:
@@ -51,89 +56,130 @@ class PeerManager:
                 break
             except ValueError:
                 continue
-            if "command" not in cmd:
-                resp = {"success": False, "error": "No command specified"}
-            elif cmd["command"] == "create":
-                resp = self.peer_create(cmd)
-            elif cmd["command"] == "delete":
-                resp = self.peer_delete(cmd)
+            try:
+                if "command" not in cmd:
+                    resp = {"success": False, "error": "No command specified"}
+                elif cmd["command"] == "bgp_update":
+                    resp = self.bgp_update(cmd)
+                elif cmd["command"] == "wg_exists":
+                    resp = self.wg_exists(cmd)
+                elif cmd["command"] == "wg_create":
+                    resp = self.wg_create(cmd)
+                elif cmd["command"] == "wg_delete":
+                    resp = self.wg_delete(cmd)
+                else:
+                    resp = {"success": False, "error": "Invalid command"}
+                self.send(resp)
+            except Exception as e:
+                logger.error(f"Failed to run command: {e}")
+                self.send({"success": False, "error": str(e)})
+
+    def wg_exists(self, info: dict) -> dict:
+        try:
+            peer_json = info["peer"]
+            peer = PeerInfo.model_validate_json(peer_json)
+            wg_if = f"wg{peer.wgid}"
+            sp = subprocess.run(["/sbin/ifconfig", wg_if], capture_output=True)
+            return {"success": not sp.returncode}
+        except Exception as e:
+            logger.error(f"Failed to check if interface exists: {e}")
+            return {"success": False, "error": str(e)}
+
+    def wg_create(self, info: dict) -> dict:
+        try:
+            peer_json = info["peer"]
+            peer = PeerInfo.model_validate_json(peer_json)
+            logger.debug("Creating peer: %s", peer)
+            peer.dn42_validate()
+            wg_if = f"wg{peer['wgid']}"
+            sp = subprocess.run(["/sbin/ifconfig", f"{wg_if}"], capture_output=True)
+            if not sp.returncode:
+                logger.error(f"Interface {wg_if} already exists")
+                return {"success": False, "error": "Interface already exists"}
+            wg_file = f"/etc/wireguard/wg{peer['wgid']}.conf"
+            wg_data = hostname_wg.render(peer=peer)
+            with open(wg_file, "w") as f:
+                f.write(wg_data)
+            sp = subprocess.run(
+                ["/bin/sh", "/etc/netstart", f"{wg_if}"], capture_output=True
+            )
+            if sp.returncode:
+                logger.error(
+                    f"Failed to create interface {wg_if}: {sp.stderr.decode()}"
+                )
+                logger.debug(f"Debug output: {sp.stdout.decode()}")
+                return {"success": False, "error": "Failed to create interface"}
+        except HTTPException as e:
+            return {"success": False, "error": e.detail}
+        except Exception as e:
+            logger.error(f"Failed to create peer: {e}")
+            return {"success": False, "error": str(e)}
+        return {"success": True}
+
+    def wg_delete(self, info: dict) -> dict:
+        try:
+            peer_json = info["peer"]
+            peer = PeerInfo.model_validate_json(peer_json)
+            logger.debug("Deleting peer: %s", peer)
+            peer.dn42_validate()
+            wg_file = f"/etc/wireguard/wg{peer['wgid']}.conf"
+            if os.path.isfile(wg_file):
+                logger.debug(f"Deleting wireguard config file {wg_file}")
+                os.unlink(wg_file)
             else:
-                resp = {"success": False, "error": "Invalid command"}
-            self.send(resp)
-
-    def peer_create(self, info: dict) -> dict:
-        # try:
-        #     wgid = info["wgid"]
-        # except KeyError:
-        #     return {"success": False, "error": "No 'wgid' specified"}
-
-        # hostname_info = hostname_wg.render()
-        # hostname_file = f"/etc/hostname.wg{wgid}"
-
-        # with open(hostname_file, "w") as f:
-        #     f.write(hostname_info)
-
-        # # test the config
-        # sp = subprocess.run(
-        #     ["/bin/sh", "/etc/netstart", "-n", f"wg{wgid}"], capture_output=True
-        # )
-        # if sp.returncode:
-        #     # remove the hostname file
-        #     os.unlink(hostname_file)
-        #     return {"success": False, "error": sp.stderr.decode()}
-
-        # # create peers BGP config
-        # bgp_peer_file = f"/etc/bgpd.d/peer-{info.new_peer.ASN}.conf"
-        # bgp_listener_file = f"/etc/bgpd.d/listener-{info.new_peer.ASN}.conf"
-
-        # with open(bgp_peer_file, "w") as f:
-        #     f.write(bgp_peer.render(peers=[info.new_peer]))
-
-        # with open(bgp_listener_file, "w") as f:
-        #     f.write(bgp_listener.render(peers=[info.new_peer]))
-
+                logger.warning(f"Wireguard hostname file {wg_file} does not exist")
+            wg_if = f"wg{peer['wgid']}"
+            sp = subprocess.run(["/sbin/ifconfig", f"{wg_if}"], capture_output=True)
+            if not sp.returncode:
+                sp = subprocess.run(
+                    ["/sbin/ifconfig", f"{wg_if}", "destroy"], capture_output=True
+                )
+                if sp.returncode:
+                    logger.debug(
+                        f"Failed to destroy interface {wg_if}: {sp.stderr.decode()}"
+                    )
+                    logger.debug(f"Debug output: {sp.stdout.decode()}")
+                    return {"success": False, "error": "Failed to destroy interface"}
+            else:
+                logger.warning(f"Interface {wg_if} does not exist")
+        except HTTPException as e:
+            return {"success": False, "error": e.detail}
+        except Exception as e:
+            logger.error(f"Failed to delete peer: {e}")
+            return {"success": False, "error": str(e)}
         return {"success": True}
 
-    def peer_delete(self, info: dict) -> dict:
+    def bgp_update(self, info: dict) -> dict:
+        try:
+            peers_json = info["peers"]
+            peers = [PeerInfo.model_validate_json(peer) for peer in peers_json]
+            for peer in peers:
+                peer.dn42_validate()
+            bgpd_file = "/etc/bgpd.conf"
+            bgpd_tmp_file = "/tmp/bgpd.conf"
+            bgpd_data = bgpd_conf.render(peers=peers)
+            # write to temp file first
+            with open(bgpd_tmp_file, "w") as f:
+                f.write(bgpd_data)
+            # test the config
+            sp = subprocess.run(
+                ["/usr/sbin/bgpd", "-f", "-n", f"{bgpd_tmp_file}"], capture_output=True
+            )
+            if sp.returncode:
+                logger.error(f"Failed to test bgpd config: {sp.stderr.decode()}")
+                os.unlink(bgpd_tmp_file)
+                return {"success": False, "error": "Failed to test bgpd config"}
+            # move the temp file to the real file
+            os.rename(bgpd_tmp_file, bgpd_file)
+            # reload bgpd
+            sp = subprocess.run(
+                ["/usr/sbin/rcctl", "reload", "bgpd"], capture_output=True
+            )
+            if sp.returncode:
+                logger.error(f"Failed to reload bgpd: {sp.stderr.decode()}")
+                return {"success": False, "error": "Failed to reload bgpd"}
+        except HTTPException as e:
+            return {"success": False, "error": e.detail}
+        except Exception as e:
+            return {"success": False, "error": str(e)}
         return {"success": True}
-
-
-# class BGPD:
-#     @staticmethod
-#     def peers_available() -> Generator[int]:
-#         # find all file names of the form /etc/bgpd.d/peer-*.conf
-#         for file in os.listdir("/etc/bgpd.d"):
-#             if file.startswith("peer-") and file.endswith(".conf"):
-#                 try:
-#                     ASN = int(file[5:-5])
-#                     yield ASN
-#                 except ValueError:
-#                     logger.error(f"Invalid peer file: {file}")
-
-#     @staticmethod
-#     def peers_enabled() -> Generator[int]:
-#         # find all lines in /etc/bgpd.d/peers-enabled.conf
-#         with open("/etc/bgpd.d/peers-enabled.conf") as f:
-#             for line in f:
-#                 # line is of the form /etc/bgpd.d/peer-*.conf
-#                 try:
-#                     ASN = int(line[17:-5])
-#                     yield ASN
-#                 except ValueError:
-#                     logger.error(f"Invalid peer file: {line}")
-
-#     @staticmethod
-#     def peer_enable(peer: int) -> None:
-#         for asn in BGPD.peers_enabled():
-#             if asn == peer:
-#                 return
-#         with open("/etc/bgpd.d/peers-enabled.conf", "a") as f:
-#             f.write(f"/etc/bgpd.d/peer-{peer}.conf\n")
-
-#     @staticmethod
-#     def peer_disable(peer: int) -> None:
-#         enabled = list(BGPD.peers_enabled())
-#         with open("/etc/bgpd.d/peers-enabled.conf", "w") as f:
-#             for asn in enabled:
-#                 if asn != peer:
-#                     f.write(f"/etc/bgpd.d/peer-{asn}.conf\n")
