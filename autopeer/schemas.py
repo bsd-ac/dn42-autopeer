638a8664ebe81134a9bab010711a6b20e18d6bb0
epsilon-0 <epsilon-0@users.noreply.github.com>
2024-12-13 14:08:57 -0800

    validations

--- autopeer/schemas.py
+++ autopeer/schemas.py
@@ -74,10 +74,6 @@ class PeerInfo(BaseModel):
                 raise HTTPException(
                     status_code=400, detail="DN42 IPv6 address not found in body"
                 )
-        if not self.dn42_ip4 and not self.dn42_ip6:
-            raise HTTPException(
-                status_code=400, detail="DN42 IPv4 and IPv6 address not found in body"
-            )
         if self.peer_ip:
             try:
                 ipaddress.ip_address(self.peer_ip)
