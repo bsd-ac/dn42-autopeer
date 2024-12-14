8a0d867550e6cc79f0e46ad9d13e8a2d5a4e1d96
epsilon-0 <epsilon-0@users.noreply.github.com>
2024-12-14 12:28:42 -0800

    hook

--- autopeer/schemas.py
+++ autopeer/schemas.py
@@ -47,7 +47,7 @@ class PeerInfo(BaseModel):
     extended_next_hop: bool = False
 
 
-    @model_validator(mode="after")
+    # @model_validator(mode="after")
     def dn42_validate(self):
         if not self.description:
             self.description = f"Peer_{self.ASN}"
