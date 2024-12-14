952ff7ebbcd48be91b07ec6ea54f068cdfac1b90
epsilon-0 <epsilon-0@users.noreply.github.com>
2024-12-13 18:39:56 -0800

    ips

--- autopeer/templates.py
+++ autopeer/templates.py
@@ -89,10 +89,10 @@ bgpd_listener = Template(
     source="""\
 {% for peer in peers %}
 {% if peer.use_ll_ip4 %}
-listen on {{ peer.peer_ll_ip4 }} port 179
+listen on {{ peer.our_ll_ip4 }} port 179
 {% endif %}
 {% if peer.use_ll_ip6 %}
-listen on {{ peer.peer_ll_ip6 }}%wg{{ peer.wg_interface }} port 179
+listen on {{ peer.our_ll_ip6 }}%wg{{ peer.wg_interface }} port 179
 {% endif %}
 {% endfor %}
 """,
