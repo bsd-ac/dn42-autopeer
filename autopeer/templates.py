ccc95f36f334e218518f62b9584f826e54f186db
epsilon-0 <epsilon-0@users.noreply.github.com>
2024-12-15 13:35:48 -0800

    templates

--- autopeer/templates.py
+++ autopeer/templates.py
@@ -17,10 +17,10 @@ wgport {{ wg_port }}
 wgpeer {{ peer_pubkey }}{% if peer_psk is defined %} wgpsk {{ peer_psk }}{% endif %}{% if peer_ip is defined %} wgendpoint {{ peer_ip }} {{ peer_port }}{% endif %}{% if peer_ll_ip4 is defined %} wgaip {{ peer_ll_ip4 }}/32{% endif %}{% if peer_ll_ip6 is defined %} wgaip {{ peer_ll_ip6 }}/128{% endif %} wgaip {{ dn42_netspace4 }} wgaip {{ dn42_netspace6 }}
 
 {% if peer_ll_ip4 is defined %}
-!route -n -T {{ wg_rdomain }} add -inet -iface {{ peer_ll_ip4 }} {{ our_ll_ip4 }}
+!route -n -T {{ wg_rdomain }} add -inet -iface {{ peer_ll_ip4 }}/32 {{ our_ll_ip4 }}
 {% endif %}
 {% if peer_ll_ip6 is defined %}
-!route -n -T {{ wg_rdomain }} add -inet6 {{ peer_ll_ip6 }} {{ our_ll_ip6 }}%{{ wg_interface }}
+!route -n -T {{ wg_rdomain }} add -inet6 {{ peer_ll_ip6 }}/128%{{ wg_interface }} {{ our_ll_ip6 }}%{{ wg_interface }}
 {% endif %}
 !route -n -T {{ wg_rdomain }} sourceaddr -ifp lo{{ wg_rdomain }}
 """,
